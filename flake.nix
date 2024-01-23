1ba4d2d80e2333cdebb1b181e3fef089529cee3b
traxys <quentin+dev@familleboyer.net>
2024-01-24 00:22:15 +0100

    Allow to build the website with nix

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
       nativeBuildInputs = [
         pkgs.cmake
         pkgs.pkg-config
-		pkgs.fontconfig
+        pkgs.fontconfig
       ];
     in {
       devShell = pkgs.mkShell {
@@ -43,13 +43,68 @@
             pkgs.bashInteractive
             pkgs.gdb
             pkgs.trunk
-			pkgs.pandoc
+            pkgs.pandoc
             rust
           ]
           ++ nativeBuildInputs;
         LD_LIBRARY_PATH = "${pkgs.lib.strings.makeLibraryPath libs}";
-		RUST_PATH = "${rust}";
+        RUST_PATH = "${rust}";
       };
+
+      packages.web = let
+        pkg = {
+          stdenv,
+          makeRustPlatform,
+          trunk,
+          pandoc,
+          binaryen,
+          wasm-bindgen-cli,
+        }: let
+          rustPlatformWasm = makeRustPlatform {
+            rustc = rust;
+            cargo = rust;
+          };
+        in
+          stdenv.mkDerivation {
+            pname = "viz";
+            version =
+              if (self ? shortRev)
+              then self.shortRev
+              else self.dirtyShortRev;
+
+            src = ./.;
+
+            nativeBuildInputs = [
+              trunk
+              pandoc
+              wasm-bindgen-cli
+              binaryen
+              rustPlatformWasm.cargoSetupHook
+              rustPlatformWasm.cargoBuildHook
+            ];
+
+            cargoDeps = rustPlatformWasm.importCargoLock {lockFile = ./Cargo.lock;};
+
+            XDG_CACHE_HOME = "/build/cache";
+
+            buildPhase = ''
+              runHook preBuild
+
+              cargo xtask build --release
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              mv target/release/html $out
+
+              runHook postInstall
+            '';
+          };
+      in
+        pkgs.callPackage pkg {};
 
       apps.safety_parabola = flake-utils.lib.mkApp rec {
         drv = naersk'.buildPackage {
